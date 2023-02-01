125d15f19e59d01af5e1826d6001a0ee2abb08fc
Rudy van Sloten <rdvansloten@outlook.com>
2023-02-01 18:39:58 +0100

    fix versions

--- test/fixture/versions.tf
+++ test/fixture/versions.tf
@@ -5,7 +5,9 @@ terraform {
       version = ">= 3.0.0, < 4.0.0"
     }
   }
-}provider "azurerm" {
+}
+
+provider "azurerm" {
   features {
     resource_group {
       prevent_deletion_if_contains_resources = false
