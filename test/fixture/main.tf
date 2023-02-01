1ed49dd9b903520e9ede6c4a89e19e380c76a145
Rudy van Sloten <rdvansloten@outlook.com>
2023-02-01 19:25:17 +0100

    Fix subnet

--- test/fixture/main.tf
+++ test/fixture/main.tf
@@ -4,10 +4,25 @@ resource "azurerm_resource_group" "main" {
   location = "westeurope"
 }
 
+resource "azurerm_virtual_network" "main" {
+  name                = "test-terraform-azurerm-aks-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "main" {
+  name                 = "aks-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
 module "terraform-azurerm-aks" {
   source = "github.com/yunikon-dev/terraform-azurerm-aks"
 
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   dns_prefix          = "testjeaks"
+  default_node_pool_vnet_subnet_id = azurerm_subnet.main.id
 }
--- variables.tf
+++ variables.tf
@@ -717,7 +717,7 @@ variable "ultra_ssd_enabled" {
   default     = false
 }
 
-variable "vnet_subnet_id" {
+variable "default_node_pool_vnet_subnet_id" {
   type        = string
   description = "(Optional) The ID of a Subnet where the Kubernetes Node Pool should exist. Changing this forces a new resource to be created."
   default     = null
