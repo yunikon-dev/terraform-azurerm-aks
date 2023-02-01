3290c5866946799938085dc587b1fb15bc68d389
Rudy van Sloten <rdvansloten@outlook.com>
2023-02-01 18:42:12 +0100

    add dns prefix

--- test/fixture/main.tf
+++ test/fixture/main.tf
@@ -9,5 +9,5 @@ module "terraform-azurerm-aks" {
 
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
-  
+  dns_prefix          = "testjeaks"
 }
