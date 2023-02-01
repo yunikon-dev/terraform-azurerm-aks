fa70ab53e4bad1b89db322e28d3496755e7e88e7
Rudy van Sloten <rdvansloten@outlook.com>
2023-02-01 17:51:19 +0100

    remove go mod

--- test/fixture/outputs.tf
+++ test/fixture/outputs.tf
@@ -1,5 +1,5 @@
 # Test output for your Terraform module
-output "terraform-azurerm-aks_name" {
+output "terraform-azurerm-aks-name" {
   value       = module.terraform-azurerm-aks.name
   description = "Module output to test."
 }
